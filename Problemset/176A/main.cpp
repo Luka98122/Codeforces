1e5113aa680e7661caedb8fcf5424adf3852a0d6
Luka Markovic <luka98122@gmail.com>
2026-05-30 16:28:15 +0200

    176A unfinished

---
+++ Problemset/176A/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n,m,k;
+    cin >> n >> m >> k;
+    vector<vector<int>> deals;
+    for (int i =0;i<n;i++){
+        string s;
+        cin >> s;
+        for (int j =0;j<m;j++){
+            int buy,sell,quant;
+            cin >> buy >> sell >> quant;
+            
+        }
+    }
+}
