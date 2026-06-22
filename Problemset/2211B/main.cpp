8fbcbaa3e16dbe7cd6a31739cc6466d322be2c8e
Luka Markovic <luka98122@gmail.com>
2026-06-22 12:12:49 +0200

    2211B

--- Problemset/2211B/main.cpp
+++ Problemset/2211B/main.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-//INFO 11:21 - 12:12
+//INFO 11:21 - 12:12 Accepted FT
 int main () {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
